21f002fee76b1bb89cc512e64bb015bccd7d1e63
Gleiderson <gleidersondev@gmail.com>
2025-02-19 23:16:10 -0300

    Feito exercicio 01 estruturas condicionais

---
+++ 3_estruturas_condicionais_e_de_repeticao/1_estruturas_condicionais.py
@@ -0,0 +1,9 @@
+MAIOR_IDADE = 18
+
+idade = int(input("Qual a sua idade: "))
+
+if idade >= MAIOR_IDADE:
+  print("É maior de idade, pode tirar a CNH")
+
+else:
+  print("Ainda não pode tirar a CNH")
