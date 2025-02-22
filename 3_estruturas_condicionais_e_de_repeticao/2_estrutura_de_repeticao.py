933c29c4efd588578a5b2eb177fe1c7b28de5484
Gleiderson <gleidersondev@gmail.com>
2025-02-22 14:21:52 -0300

    FOR com range

--- 3_estruturas_condicionais_e_de_repeticao/2_estrutura_de_repeticao.py
+++ 3_estruturas_condicionais_e_de_repeticao/2_estrutura_de_repeticao.py
@@ -1,10 +1,30 @@
-texto = input("Informe um texto: ")
-VOGAIS = "AEIOU"
+# FOR
 
-print(texto)
+# texto = input("Informe um texto: ")
+# VOGAIS = "AEIOU"
 
-for indice in texto:
-  if indice.upper() in VOGAIS:
-    print(indice, end=" ")
+# print(texto)
 
-print()
+# for indice in texto:
+#   if indice.upper() in VOGAIS:
+#     print(indice, end=" ")
+
+# print()
+
+# FOR / ELSE
+
+# numeros = [1, 3, 5, 7, 9]
+
+# for num in numeros:
+#     if num == 3:
+#         print("Número encontrado!")
+#         break  # Interrompe o loop se encontrar
+# else:
+#     print("Número não encontrado.")  # Executa se o break NÃO for acionado
+
+# FOR com RANGE
+# tabuada do cinco
+for i in range(0, 51, 5):
+  print(i, end=",")
+
+
