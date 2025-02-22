0b320ae7ca1f6942c4df81fd8c9cf120b6d0e89f
Gleiderson <gleidersondev@gmail.com>
2025-02-22 16:00:08 -0300

    Terminado estrutura de repetição

--- 3_estruturas_condicionais_e_de_repeticao/2_estrutura_de_repeticao.py
+++ 3_estruturas_condicionais_e_de_repeticao/2_estrutura_de_repeticao.py
@@ -1,31 +1,32 @@
 # FOR
 
-# texto = input("Informe um texto: ")
-# VOGAIS = "AEIOU"
+texto = input("Informe um texto: ")
+VOGAIS = "AEIOU"
 
-# print(texto)
+print(texto)
 
-# for indice in texto:
-#   if indice.upper() in VOGAIS:
-#     print(indice, end=" ")
+for indice in texto:
+  if indice.upper() in VOGAIS:
+    print(indice, end=" ")
 
-# print()
+print()
 
 # FOR / ELSE
 
-# numeros = [1, 3, 5, 7, 9]
+numeros = [1, 3, 5, 7, 9]
 
-# for num in numeros:
-#     if num == 3:
-#         print("Número encontrado!")
-#         break  # Interrompe o loop se encontrar
-# else:
-#     print("Número não encontrado.")  # Executa se o break NÃO for acionado
+for num in numeros:
+    if num == 3:
+        print("Número encontrado!")
+        break  # Interrompe o loop se encontrar
+else:
+    print("Número não encontrado.")  # Executa se o break NÃO for acionado
 
 # FOR com RANGE
 # tabuada do cinco
-# for i in range(0, 51, 5):
-#   print(i, end=",")
+
+for i in range(0, 51, 5):
+  print(i, end=",")
 
 # while
 
@@ -46,4 +47,20 @@ while opcao != 0:
 
     except ValueError:
         print("Erro! Digite um número válido.")
+
+senha = "python123"
+entrada = ""
+
+while entrada != senha:
+    entrada = input("Digite a senha: ")
+
+print("Acesso permitido!")
+
+contador = [1, 8, 10, 16, 24]
+
+while contador == 16:
+  print("contem {contador}")
+
+print()
+
 
