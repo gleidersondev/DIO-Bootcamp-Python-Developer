3a898df5c4be227bf429070958ffb76b0a0965eb
Gleiderson <gleidersondev@gmail.com>
2025-02-22 17:34:34 -0300

    Exercicio 1 terminado

---
+++ 4_exercicios/exercicio_1.py
@@ -0,0 +1,11 @@
+# Peça ao usuário para digitar dois números e compare se o primeiro é maior que o segundo.
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+if num1 > num2:
+  print("O primeiro é maior!")
+elif num2 > num1:
+  print("O segundo é o maior!")
+else:
+  print("Os dois são Iguais!")
+ 
