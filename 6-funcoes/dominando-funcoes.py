862976119fc8682f320806ca7bef3b64f1761761
Gleiderson <gleidersondev@gmail.com>
2025-02-27 23:09:43 -0300

    Retorno de valores

--- 6-funcoes/dominando-funcoes.py
+++ 6-funcoes/dominando-funcoes.py
@@ -9,3 +9,17 @@ def exibir_mensagem_2(nome):
 
 exibir_mensagem_2(nome="Gleiderson")
 
+# Retorno de valores
+
+def calcular_total(numeros):
+    return sum(numeros)
+
+
+def retorna_antecessor_e_sucessor(numero):
+    antecessor = numero - 1
+    sucessor = numero + 1
+
+    return antecessor, sucessor
+
+print(calcular_total([1,2,3]))
+print(retorna_antecessor_e_sucessor(10))
