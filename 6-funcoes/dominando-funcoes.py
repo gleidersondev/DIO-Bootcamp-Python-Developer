6f8f66226ab99c10c4b59669db467ef426fc4fec
Gleiderson <gleidersondev@gmail.com>
2025-02-27 23:05:11 -0300

    Primeira função

---
+++ 6-funcoes/dominando-funcoes.py
@@ -0,0 +1,11 @@
+# Primeira função
+def exibir_mensagem():
+    print("Olá mundo!")
+
+exibir_mensagem()
+
+def exibir_mensagem_2(nome):
+    print(f"Seja bem vindo {nome}!")
+
+exibir_mensagem_2(nome="Gleiderson")
+
