1639dab876024b6b70cf4ac4b352f9492cbfdc43
Gleiderson <gleidersondev@gmail.com>
2025-03-11 20:13:29 -0300

    Trabalhando com listas: Matriz e Iteração

--- 5-Trabalhando-com-listas-python/5.1-Trabalhando-com-listas/listas.py
+++ 5-Trabalhando-com-listas-python/5.1-Trabalhando-com-listas/listas.py
@@ -6,3 +6,27 @@ frutas = ["maçã", "laranja", "uva", "pera"]
 
 print(frutas[2])
 print(frutas[-1])
+
+# Matriz
+
+matriz = [
+    [1, "a", 2],
+    ["b", 3, 4],
+    [6, 5, "c"]
+]
+
+print(matriz[0]) 
+print(matriz[0][0])
+print(matriz[0][-1])
+print(matriz[-1][-1])
+
+# Iteração
+
+carros = ["gol", "celta", "palio"]
+
+for carro in carros:
+    print(carro)
+
+
+for indice, carro in enumerate(carros):
+    print(f"{indice}: {carro}")
