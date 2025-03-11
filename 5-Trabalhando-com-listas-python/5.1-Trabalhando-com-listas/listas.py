6aa3276bc4d5a1bb8844e796e48209e322298784
Gleiderson <gleidersondev@gmail.com>
2025-03-11 20:16:36 -0300

    Trabalhando com listas: Compressão

--- 5-Trabalhando-com-listas-python/5.1-Trabalhando-com-listas/listas.py
+++ 5-Trabalhando-com-listas-python/5.1-Trabalhando-com-listas/listas.py
@@ -30,3 +30,15 @@ for carro in carros:
 
 for indice, carro in enumerate(carros):
     print(f"{indice}: {carro}")
+
+# Compressão
+
+numeros = [1, 30, 21, 2, 9, 65, 34]
+pares = [numero for numero in numeros if numero % 2 == 0]
+print(pares)
+
+# Modificar valores
+
+numeros = [1, 30, 21, 2, 9, 65, 34]
+quadrado = [numero**2 for numero in numeros]
+print(quadrado)
