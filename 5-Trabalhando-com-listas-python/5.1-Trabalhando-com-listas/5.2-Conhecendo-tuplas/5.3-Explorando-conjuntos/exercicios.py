1ab99307e65924518a6a1080b38e89ec5801c74f
Gleiderson <gleidersondev@gmail.com>
2025-03-13 22:50:53 -0300

    Inclusão de exercicios de conjunto

---
+++ 5-Trabalhando-com-listas-python/5.1-Trabalhando-com-listas/5.2-Conhecendo-tuplas/5.3-Explorando-conjuntos/exercicios.py
@@ -0,0 +1,18 @@
+# 1️⃣ Controle de Usuários Cadastrados
+
+# 📌 Situação: Você tem um sistema e deseja verificar se um novo usuário já está cadastrado.
+
+# 🔹 Desafio:
+
+#        Crie um conjunto de usuários cadastrados e um novo conjunto com usuários que tentaram se registrar.
+
+#        Exiba quais usuários já estão cadastrados e quais são novos
+
+
+# Exemplo:
+
+usuarios_cadastrados = {"alice", "bob", "carla", "daniel"}
+novos_usuarios = {"bob", "daniel", "eva", "fernando"}
+
+# Verificar quais já estão cadastrados
+
