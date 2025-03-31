9a943e0bb318fea7ee306ab9cae86a27e3a73151
Gleiderson <gleidersondev@gmail.com>
2025-03-31 20:44:10 -0300

    Exercicio de cadastro de empresa

--- 6-Programacao-orientada-a-objetos/6.1-Introducao-poo/3_cadastro_empresa.py
+++ 6-Programacao-orientada-a-objetos/6.1-Introducao-poo/3_cadastro_empresa.py
@@ -5,8 +5,26 @@ class Empresa:
     self.faturamento = faturamento
     
   def calcular_imposto(self):
-    bc = (float(self.faturamento) * 0.32) * 0.15
-    irpj = ((bc - 20000) * 0.10) + bc if bc > 20000 else bc
+    bc = (float(self.faturamento) * 0.32)
+    irpj_normal = bc * 0.15
+    irpj = ((irpj_normal - 20000) * 0.10) + irpj_normal if irpj_normal > 20000 else bc 
+    csll = bc * 0.09
+    return irpj, csll
+  
+  def exibir_dados(self):
+    irpj, csll = self.calcular_imposto()
+    print(f"""
+    A empresa {self.nome}, inscrita no CNPJ nº {self.cnpj}, apresentou o seguinte faturamento no período:
+    
+    R$ {float(self.faturamento):.2f}.
+    
+    O imposto devido foi:
+    IRPJ R$ {irpj:.2f}
+    CSLL R$ {csll:.2f}
+    
+    """)
+    
+
   
   
       
