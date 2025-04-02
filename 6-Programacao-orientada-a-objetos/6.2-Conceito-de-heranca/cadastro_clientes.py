4a4d22fa51ae74c7d8bc68f46a9219c513bf271e
Gleiderson <gleidersondev@gmail.com>
2025-04-02 20:47:33 -0300

    Corrigindo class Funcionário

--- 6-Programacao-orientada-a-objetos/6.2-Conceito-de-heranca/cadastro_clientes.py
+++ 6-Programacao-orientada-a-objetos/6.2-Conceito-de-heranca/cadastro_clientes.py
@@ -8,3 +8,16 @@ class Funcionario(Pessoa):
     Pessoa.__init__(self, nome, idade)
     self.cargo = cargo
     self.salario = salario
+    
+class Beneficios:
+  def __init__(self, vale_transporte, vale_alimentacao):
+    self.vale_transporte = vale_transporte
+    self.vale_alimentacao = vale_alimentacao
+    
+class Gerente(Funcionario, Beneficios):
+  def __init__(self, nome, cargo, salario, vale_transporte, vale_alimentacao):
+    Funcionario.__init__(self, cargo, salario)
+    Beneficios.__init__(self, vale_transporte, vale_alimentacao)
+    self.nome = nome
+    
+    def exibir_dados(self):
