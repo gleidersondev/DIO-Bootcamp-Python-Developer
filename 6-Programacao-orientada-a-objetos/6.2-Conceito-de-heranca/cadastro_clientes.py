94053852bb4f630da6154eece4feeb2ee94c1b46
Gleiderson <gleidersondev@gmail.com>
2025-04-02 21:39:47 -0300

    Exercicio terminado

--- 6-Programacao-orientada-a-objetos/6.2-Conceito-de-heranca/cadastro_clientes.py
+++ 6-Programacao-orientada-a-objetos/6.2-Conceito-de-heranca/cadastro_clientes.py
@@ -5,19 +5,32 @@ class Pessoa:
     
 class Funcionario(Pessoa):
   def __init__(self, nome, idade, cargo, salario):
-    Pessoa.__init__(self, nome, idade)
+    super().__init__(self, nome, idade)
     self.cargo = cargo
     self.salario = salario
     
 class Beneficios:
-  def __init__(self, vale_transporte, vale_alimentacao):
-    self.vale_transporte = vale_transporte
-    self.vale_alimentacao = vale_alimentacao
+  def __init__(self):
+    self.vale_transporte = 500
+    self.vale_alimentacao = 1000
     
 class Gerente(Funcionario, Beneficios):
-  def __init__(self, nome, cargo, salario, vale_transporte, vale_alimentacao):
-    Funcionario.__init__(self, cargo, salario)
-    Beneficios.__init__(self, vale_transporte, vale_alimentacao)
-    self.nome = nome
+  def __init__(self, nome, idade, salario):
+    Funcionario.__init__(self, nome, idade, "Gerente", salario)
+    Beneficios.__init__(self)
+    self.bonus = self.salario * 0.10
+  
     
     def exibir_dados(self):
+      print(f"""
+      
+      Nome do funcionário: {self.nome}
+      Idade: {self.idade}
+      Cargo: {self.cargo}
+      Salário: R$ {self.salario:.2f}
+      Bônus: R$ {self.bonus:.2f}
+      Vale Transporte: R$ {self.vale_transporte:.2f}
+      Vale Alimentação: R$ {self.vale_alimentacao:.2f}
+      
+            """)
+      
